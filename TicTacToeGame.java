83654aee3a7c971dcc078a7e66741644b6e8661d
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-08 11:31:49 +0530

    MaheswariKoyyani : Print Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,38 +4,43 @@ package com.capgemini.tictac;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	public static void main(String[] args) {
-        char computerLetter;
-        char userinput = chooseLetter();
-        if(userinput=='X')
-        	computerLetter='O';
-        else
-        	computerLetter='X';
-		createBoard();
-		
-	}
-
-	public static void createBoard() {
+	
+	public static char[] createBoard() {
 		char[] board = new char[10];
 		for (int i = 0; i < 10; i++) {
 			board[i] = ' ';
 		}
+		return board;
 	}
-	//usecase2//
+	// usecase2//
 
 	public static char chooseLetter() {
 		System.out.println("Enter the Letter:");
 		Scanner sc = new Scanner(System.in);
-		char computerLetter;
 		char letter = sc.next().charAt(0);
 		return letter;
-}
+	}
+
 	public static void showBoard(char[] board) {
 
-		System.out.println("|"  +board[0]+ "|" +board[1]+ "|"+board[2]+ "|");
+		System.out.println("|" + board[0] + "|" + board[1] + "|" + board[2] + "|");
 		System.out.println("----------");
-		System.out.println("|"  +board[3]+ "|" +board[4]+ "|"+board[5]+ "|");
+		System.out.println("|" + board[3] + "|" + board[4] + "|" + board[5] + "|");
 		System.out.println("----------");
-		System.out.println("|"  +board[6]+ "|" +board[7]+ "|"+board[8]+ "|");
+		System.out.println("|" + board[6] + "|" + board[7] + "|" + board[8] + "|");
+	}
+	public static void main(String[] args) {
+		char computerLetter;
+		char[] board=createBoard();
+		char userinput = chooseLetter();
+		if (userinput == 'X')
+			computerLetter = 'O';
+		else
+			computerLetter = 'X';
+		createBoard();
+		showBoard(board);
+	}
+	private static void showBoard(Object board) {
+		
 	}
 }
