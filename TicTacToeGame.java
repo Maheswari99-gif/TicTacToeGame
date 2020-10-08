59ded34f29d6c860e828289a79d09779b0111a9e
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-08 10:37:04 +0530

    MaheswariKoyyani : Choosing a Letter

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,29 +4,30 @@ package com.capgemini.tictac;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	 public static void main(String[] args) {
-		 TicTacToeGame obj =new TicTacToeGame();
-		 obj.createBoard();
-		obj.chooseLetter();
-		 
-	 }
-	 public static void createBoard() {
-		 char[] board =new char[10];
-		 for(int i=0;i<10;i++) {
-			 board[i]=' ';
-		 }
-	 }
-     public static void chooseLetter() {
-    	 System.out.println("Enter the Letter:");
-    	 Scanner s= new Scanner(System.in);
-    	 char c=s.next().charAt(0);
-    	 if(c=='X') {
-    		 System.out.println("Computer has choosen O");
-    	 }
-    	 else
-    		 System.out.println("Computer has choosen X");
-    		 
-    	 
-    	 
-     }
+	public static void main(String[] args) {
+        char computerLetter;
+        char Letter = chooseLetter();
+        if(Letter=='X')
+        	computerLetter='O';
+        else
+        	computerLetter='X';
+		createBoard();
+		
+	}
+
+	public static void createBoard() {
+		char[] board = new char[10];
+		for (int i = 0; i < 10; i++) {
+			board[i] = ' ';
+		}
+	}
+	//usecase2//
+
+	public static char chooseLetter() {
+		System.out.println("Enter the Letter:");
+		Scanner sc = new Scanner(System.in);
+		char computerLetter;
+		char letter = sc.next().charAt(0);
+		return letter;
+}
 }
