41c50553cda3cd341ac263bd563e682c0655b084
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-08 11:02:52 +0530

    MaheswariKoyyani : Print Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	public static void main(String[] args) {
         char computerLetter;
-        char Letter = chooseLetter();
-        if(Letter=='X')
+        char userinput = chooseLetter();
+        if(userinput=='X')
         	computerLetter='O';
         else
         	computerLetter='X';
@@ -30,4 +30,12 @@ public class TicTacToeGame {
 		char letter = sc.next().charAt(0);
 		return letter;
 }
+	public static void showBoard(char[] board) {
+
+		System.out.println("|"  +board[0]+ "|" +board[1]+ "|"+board[2]+ "|");
+		System.out.println("----------");
+		System.out.println("|"  +board[3]+ "|" +board[4]+ "|"+board[5]+ "|");
+		System.out.println("----------");
+		System.out.println("|"  +board[6]+ "|" +board[7]+ "|"+board[8]+ "|");
+	}
 }
